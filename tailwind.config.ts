91de630b647582296ce2ad5218ce0e38babcd3e2
piotr.marganski <piotr.marganski@summer.agency>
2024-11-28 10:12:26 +0100

    Styling configuration - customize tailwind config up to system design, adjust base files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      xs: '12px',
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '20px',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        gray: {
+          50: '#F9FAFB',
+          100: '#F5F5F5',
+          200: '#EAECF0',
+          300: '#D0D5DD',
+          600: '#667085',
+          700: '#475467',
+          800: '#344054',
+          900: '#101828',
+        },
+        purple: {
+          300: '#D6BBFB',
+          500: '#7F56D9',
+          600: '#6941C6',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+        inter: ['var(--font-inter)', 'sans-serif'],
       },
     },
   },
